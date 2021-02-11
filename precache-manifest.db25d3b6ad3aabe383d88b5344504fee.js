236a9f6bfc1b7d01867bf122efaaae2d95a395d0
mobara121 <mizueo121@gmail.com>
2021-02-11 12:09:43 -0500

    Updates

---
+++ precache-manifest.db25d3b6ad3aabe383d88b5344504fee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55506a4f2ac29c49c9a5260fd014f55",
+    "url": "/portfolio_rev6/index.html"
+  },
+  {
+    "revision": "efa5c68b4a942b4d342b",
+    "url": "/portfolio_rev6/static/css/main.d1853049.chunk.css"
+  },
+  {
+    "revision": "547a98d654e6c2a23e61",
+    "url": "/portfolio_rev6/static/js/2.ae0b8ef7.chunk.js"
+  },
+  {
+    "revision": "efa5c68b4a942b4d342b",
+    "url": "/portfolio_rev6/static/js/main.5e7f85b9.chunk.js"
+  },
+  {
+    "revision": "a14b9434ae9a9ab7b4c4",
+    "url": "/portfolio_rev6/static/js/runtime~main.7b03f9ce.js"
+  },
+  {
+    "revision": "72abfc8f8571d61aeca17940b82490cd",
+    "url": "/portfolio_rev6/static/media/Gurunavi.72abfc8f.png"
+  },
+  {
+    "revision": "1b93ed3cd34509d68b33c459bbb18900",
+    "url": "/portfolio_rev6/static/media/Real_Estate_Agent.1b93ed3c.png"
+  },
+  {
+    "revision": "6e6c0a0f775e49934f4ae456eabff1c6",
+    "url": "/portfolio_rev6/static/media/Snapshot.6e6c0a0f.PNG"
+  },
+  {
+    "revision": "5777e58584b7cff691869319d74c0813",
+    "url": "/portfolio_rev6/static/media/calorywatcher.5777e585.png"
+  },
+  {
+    "revision": "48a112d1a08ba1c8c458d6ca23f1b375",
+    "url": "/portfolio_rev6/static/media/nyt.48a112d1.png"
+  },
+  {
+    "revision": "d710abadca8b50d3423a89aacb05edbc",
+    "url": "/portfolio_rev6/static/media/today_s_menu.d710abad.png"
+  }
+]);
